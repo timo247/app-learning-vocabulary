adc29e774061d2616dce3a0e51c5f43859a13ae6
Timothée Dione <timothee.dione@gmail.com>
2024-09-16 19:36:36 +0200

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,7 @@ use App\Http\Controllers\VocabularyController;
 
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::post('vocabularies', [VocabularyController::class, 'redirectToVocabulariesList']);
     Route::post('/update', [VocabularyController::class, 'update']);
     Route::post('/create', [VocabularyController::class, 'store']);
     Route::delete('/delete', [VocabularyController::class, 'destroy']);
 });
-
-Route::post("login", [UserController::class, 'authenticate'])->name('authenticate');
-Route::post("logout", [UserController::class, 'logout']);
-// Route::get('/login', function () {
-//     return view('auth.login');
-// });
